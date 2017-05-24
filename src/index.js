4bf26665cd9d79c76cc42e3a3622ae882d5fba73
avmcrae <avalonmcrae@gmail.com>
2017-05-24 16:04:57 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import BookList from './BookList';
+
+ReactDOM.render(
+	<BookList name="React" />,
+	document.getElementById("root")
+);
